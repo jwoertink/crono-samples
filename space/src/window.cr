c37673172eaddbd1e6849cb1f81825bd7748402b
Jeremy Woertink <jeremywoertink@gmail.com>
2017-06-06 19:47:11 -0700

    renderig the score on the screen

--- space/src/window.cr
+++ space/src/window.cr
@@ -1,10 +1,15 @@
 class Window < Crono::Window
   @player : Player?
   @background_image : Crono::Image?
+  @score_text : Crono::Font
 
   def initialize
     initialize(640, 480)
     @stars = [] of Star
+    font_path = File.join(__DIR__, "assets", "fonts", "zerovelo.ttf")
+    @score_text = Crono::Font.new(font_path, 18)
+    @score_text.text = "Score: 0"
+    @score_text.color = Crono::Color::YELLOW
   end
 
   def after_init
@@ -21,18 +26,20 @@ class Window < Crono::Window
   # The order of drawing is the z-index 
   def draw
     brush.draw(@background_image.not_nil!, {0, 0})
-    @stars.each { |star| brush.draw(star.image, {star.x, star.y}) }
+    @stars.each { |star| 
+      brush.animate(star.image, {star.x, star.y}) 
+    }
     brush.draw(player.image, {player.x, player.y}, player.angle)
-    #@font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    brush.draw(@score_text, {10, 10})
   end
 
   def update
     player.move
     player.collect_stars(@stars)
+    @score_text.text = "Score: #{player.score}"
     if rand(100) < 4 && @stars.size < 25
       @stars.push(Star.new)
     end
-    puts player.score
   end
 
   def key_pressed(key)
